c70ef4f772fbb1e9d1d2d0fa1bb4459049b2f6c3
Jeremiah Bintang Santoso <ge83gof@mytum.de>
2024-11-24 04:46:17 +0100

    Fix again

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@
 const nextConfig = {
   output: 'export',
   basePath: '/erec-2025',
+  assetPrefix: '/erec-2025/',
   images: {
     unoptimized: true, // For static exports, images must be unoptimized
   },
