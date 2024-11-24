bd23d58cd023ceb467a953623f599882f716d206
Jeremiah Bintang Santoso <ge83gof@mytum.de>
2024-11-24 05:24:38 +0100

    Fix styling

--- src/app/comingSoon/details.tsx
+++ src/app/comingSoon/details.tsx
@@ -4,39 +4,41 @@ import styles from "./comingSoon.module.css";
 export default function Details() {
   return (
     <div className={styles.detailsContainer}>
-        <div className={styles.label}>
-        Date
-        </div>
-        <div className={styles.text}>
-        18-21 April 2025
-        </div>
-        <div className={styles.label}>
-        Venue
-        </div>
-        <div className={styles.text}>
-        DJH Jugendherberge Halle, <br/>
-        Große Steinstraße 60, <br/>
-        06108 Halle (Saale), <br/>
-        Germany                   
-        <a href="https://maps.app.goo.gl/R1rfbYq1dNcG7WoS7" target="_blank" >
-        <Image
-            width={40}
-            height={40}
-            className={styles.arrowCircleRightIcon}
-            alt="Arrow Circle Right"
-            src="/erec-2025/icons/arrowCircleRight.svg"
-        />
+      <div className={styles.label}>Date</div>
+      <div className={styles.text}>18-21 April 2025</div>
+      <div className={styles.label}>Venue</div>
+      <div className={styles.text}>
+        DJH Jugendherberge Halle, <br />
+        Große Steinstraße 60, <br />
+        06108 Halle (Saale), <br />
+        Germany
+        <a href="https://maps.app.goo.gl/R1rfbYq1dNcG7WoS7" target="_blank">
+          <div className={styles.arrowCircleRightIcon}>
+            <Image
+              fill
+              objectFit="contain"
+              alt="Arrow Circle Right"
+              src="/erec-2025/icons/arrowCircleRight.svg"
+            />
+          </div>
         </a>
-        </div>
-        <hr style={{
-            marginBottom: "40px",
-        }}/>
-        <div className={styles.label}>
-            CONTACT
-        </div>
-        <div className={styles.contact}>
-            Jeremiah <a className={styles.link} href="https://wa.me/+4915236823900" target="_blank">+49 152 36823900</a>
-        </div>
+      </div>
+      <hr
+        style={{
+          marginBottom: "40px",
+        }}
+      />
+      <div className={styles.label}>CONTACT</div>
+      <div className={styles.contact}>
+        Jeremiah{" "}
+        <a
+          className={styles.link}
+          href="https://wa.me/+4915236823900"
+          target="_blank"
+        >
+          +49 152 36823900
+        </a>
+      </div>
     </div>
   );
 }
